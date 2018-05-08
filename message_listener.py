8aa349c71253a06ae873faef50b8f69fcb240c1a
Muhammad Haroon <m_haroon96@hotmail.com>
2018-05-08 16:26:39 +0500

    added update command

--- message_listener.py
+++ message_listener.py
@@ -24,7 +24,7 @@ def handle(msg):
 	id = msg['chat']['id']
 	if 'youtu.be' in text or 'youtube' in text:
 		sendMessage(id, 'Processing YouTube link')
-		youtube_handler.handle(id, text)
+		subprocess.Popen('youtube-dl -x --audio-format mp3 --audio-quality 0 --exec "python3 send_audio.py ' + str(id) + ' {}" ' + text, shell=True)
 	else:
 		cmd = text.lower().strip()
 		if 'rms' == cmd:
@@ -33,11 +33,15 @@ def handle(msg):
 		elif 'status' == cmd:
 			sendMessage(id, 'Haroon-Pi is running')
 		elif 'reboot' == cmd:
-			sendMessage(id, 'Haroon-Pi is running')
+			sendMessage(id, 'Rebooting...')
 			os.popen("sudo -S reboot", 'w').write(open('pass').read())
 		elif 'help' == cmd:
 			sendMessage(id, 'status, reboot, rms, help')
-	
+		elif 'update' == cmd:
+			sendMessage(id, 'Pulling updates from GitLab...')
+			subprocess.Popen('git pull https://haroon96:EchoFoxtrot96@gitlab.com/haroon96/HaroonPiBot')
+			os.popen("sudo -S reboot", 'w').write(open('pass').read())
+			
 def main():
 	os.chdir(sys.path[0])
 	bot = create_bot(get_token())
--- youtube_handler.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-
-def handle(id, text):
-	subprocess.Popen('youtube-dl -x --audio-format mp3 --audio-quality 0 --exec "python3 send_audio.py ' + str(id) + ' {}" ' + text, shell=True)
